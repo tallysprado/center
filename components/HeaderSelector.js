fd6e2bfbe6985e6f630b80be8bc0a10772741fe2
Tallys Prado <tallys.prado@gmail.com>
2020-01-16 00:18:58 -0300

    header

--- components/HeaderSelector.js
+++ components/HeaderSelector.js
@@ -1,7 +1,47 @@
 import React from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { ACTIVE_MALE } from "../stores/GenderReducer"
-import { Header } from 'react-native-elements'
+import { Header, LinearGradient, Text } from 'react-native-elements'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {StyleSheet, Image} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+
+
+
+const LeftButton = () => {
+    return(
+        <TouchableOpacity>
+            <Icon
+                style={styles.icon}
+                name="venus-mars"
+                size={32}
+                color="#fff"
+            />
+        </TouchableOpacity>
+    )
+}
+
+const RightButton = () => {
+    return(
+        <TouchableOpacity>
+            <Icon
+                style={styles.icon}
+                name="tshirt"
+                size={32}
+                color="#fff"
+            />
+        </TouchableOpacity>
+    )
+}
+
+const CenterLogo = () => {
+    return(
+        <Image
+            source={require('../assets/centerlogo.png')}
+            style={styles.image}
+        />
+    )
+}
 
 const HeaderSelector = ({ navigation }) => {
     const gender = useSelector(state => state.gender);
@@ -9,9 +49,11 @@ const HeaderSelector = ({ navigation }) => {
     dispatch({ type: ACTIVE_MALE })
     return(
         <Header 
-            leftComponent={{icon: 'tshirt', text: 'Gender'}}
-            centerComponent={{text: 'CENTER FASHION LOGO'}}
-            rightComponent={{icon: 'tshirt', text: 'Vestuário'}}
+            
+            containerStyle = {styles.header}
+            leftComponent={<LeftButton/>}
+            centerComponent={<CenterLogo/>}
+            rightComponent={<RightButton/>}
             placement="center"
         />
     )
@@ -19,6 +61,32 @@ const HeaderSelector = ({ navigation }) => {
 
 HeaderSelector.navigationOptions = {
     title: 'Home',
+    
+    headerBackground: (
+        <LinearGradient
+          colors={['#4cbdd7', '#3378C3']}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 1, y: 1 }}
+          style={{ flex: 1 }}
+        />
+    ),
+
 };
+
+const styles = StyleSheet.create({
+    header: {
+        paddingTop: 40,
+        height: 80,
+    },
+    icon: {
+        paddingBottom: 50,
+    },
+    image: {
+        height: 80,
+        width: 80,
+        resizeMode: 'contain'
+    }
+})
+
 
 export default HeaderSelector
